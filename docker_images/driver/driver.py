4cb874a877f6e3a9eff8598741562a89ae6f5d5b
patbrown1357 <patbrown1357@gmail.com>
2021-10-27 15:42:10 -0400

    dockerfiles

---
+++ docker_images/driver/driver.py
@@ -0,0 +1,14 @@
+def main():
+    print("Welcome to Big Data Processing Application")
+    print("Please enter a number to run an application:")
+    print("1) Apache Hadoop")
+    print("2) Apache Spark")
+    print("3) Jupyter Notebook")
+    print("4) SonarQube and SonarScanner")
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
